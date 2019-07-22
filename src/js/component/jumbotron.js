b9a61b3abe80f9fca40cff4a9dbc27c48fea59ef
Keiji Nakagawa <kenaga9@icloud.com>
2019-07-22 22:50:10 +0000

    carrusel

--- src/js/component/jumbotron.js
+++ src/js/component/jumbotron.js
@@ -4,34 +4,48 @@ import { Link } from "react-router-dom";
 export default class Jumbotron extends React.Component {
 	render() {
 		return (
-			<div
-				id="jumbotron"
-				className="card card-image"
-				style={{
-					backgroundImage:
-						"url(http://images.pushsquare.com/69443f19f8f70/avengers-endgame-marvels-avengers-ps4-playstation-4.original.jpg)"
-				}}>
-				<div className="container d-flex h-100">
-					<div className="row justify-content-center align-self-center">
-						<div className="row">
-							<div id="infojumbo" className=" col-6">
-								<p>ALL EPISODES NOW STREAMING</p>
-								<h1>Catch-22</h1>
-								<h2>S1 E1 - Episode 1</h2>
-								<h5>
-									Young American flyers arrive in war and discover that the bureaucracy is more deadly
-									than the enemy.
-								</h5>
-								<span>
-									<Link to={"/search"}>
-										<i className="text-white far fa-play-circle  fa-3x mr-3" />
-									</Link>
-									START WATCHING
-								</span>
+			<div>
+				<div id="carouselExampleFade" className="carousel slide carousel-fade" data-ride="carousel">
+					<div className="carousel-inner">
+						<div className="carousel-item active">
+							<img
+								className="d-block w-100"
+								src="https://mdbootstrap.com/img/Photos/Slides/img%20(15).jpg"
+								alt="First slide"
+							/>
+							<div id="carousel" className="carousel-caption d-none d-md-block">
+								<div className="container d-flex h-100">
+									<div className="row justify-content-center align-self-center">
+										<div className="row">
+											<div id="infojumbo" className=" col-6">
+												<h1>Catch-22</h1>
+												<h2>S1 E1 - Episode 1</h2>
+												<h5>
+													Young American flyers arrive in war and discover that the
+													bureaucracy is more deadly than the enemy.
+												</h5>
+												<span>
+													<Link to={"/search"}>
+														<i className="text-white far fa-play-circle  fa-3x mr-3" />
+													</Link>
+													START WATCHING
+												</span>
+											</div>
+										</div>
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
+				<a className="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+					<span className="carousel-control-prev-icon" aria-hidden="true" />
+					<span className="sr-only">Previous</span>
+				</a>
+				<a className="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+					<span className="carousel-control-next-icon" aria-hidden="true" />
+					<span className="sr-only">Next</span>
+				</a>
 			</div>
 		);
 	}
